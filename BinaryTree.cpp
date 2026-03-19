49803e214b60657ab6299ca92749b8afd74bf830
ToombaYoomba <125666864+ToombaYoomba@users.noreply.github.com>
2026-03-19 23:55:01 +0300

    Code beautify

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -11,66 +11,31 @@ public:
     Node(int num) : value(num), left(nullptr), right(nullptr), parent(nullptr) {}
 };
 
-class BinaryTree {
-public:
-    Node* root;
-
-    BinaryTree(Node* starting_node) : root(starting_node) {}
-
-    void rewireNode(Node* new_parent_Node, Node* new_child_Node, int direction){
-        if (new_parent_Node == nullptr){
-                new_child_Node->parent = nullptr;
-                return;
-        }
-        if (direction == 0){
-            if (new_child_Node == nullptr){
-                new_parent_Node->left = nullptr;
-                return;
-            }
-            new_parent_Node->left = new_child_Node;
-            new_child_Node->parent = new_parent_Node;
-        }
-        else{
-            if (new_child_Node == nullptr){
-                new_parent_Node->right = nullptr;
-                return;
-            }
-            new_parent_Node->right = new_child_Node;
-            new_child_Node->parent = new_parent_Node;
-        }
+void rewireNode(Node* new_parent_Node, Node* new_child_Node, int direction){
+    if (new_parent_Node == nullptr){
+            new_child_Node->parent = nullptr;
+            return;
     }
-
-    void addNode(int num){
-        Node* newNode = new Node(num);
-
-        if (root == nullptr){
-            root = newNode;
+    if (direction == 0){
+        if (new_child_Node == nullptr){
+            new_parent_Node->left = nullptr;
             return;
         }
-
-        Node* curr_Node = root;
-        while (1){
-            if (num >= curr_Node->value){
-                if (curr_Node->right == nullptr){
-                    rewireNode(curr_Node, newNode, 1);
-                    return;
-                }
-                else{
-                    curr_Node = curr_Node->right;
-                }
-            }
-            else{
-                if (curr_Node->left == nullptr){
-                    rewireNode(curr_Node, newNode, 0);
-                    return;
-                }
-                else{
-                    curr_Node = curr_Node->left;
-                }
-            }
-        }
+        new_parent_Node->left = new_child_Node;
+        new_child_Node->parent = new_parent_Node;
     }
+    else{
+        if (new_child_Node == nullptr){
+            new_parent_Node->right = nullptr;
+            return;
+        }
+        new_parent_Node->right = new_child_Node;
+        new_child_Node->parent = new_parent_Node;
+    }
+}
 
+class BinaryTree {
+private:
     Node* findRecursive(Node* curr_Node, int num) {
         if (curr_Node == nullptr) {
             return nullptr;
@@ -121,6 +86,54 @@ public:
         }
     }
 
+    bool isMirror(Node* leftNode, Node* rightNode) {
+        if (leftNode == nullptr && rightNode == nullptr){ 
+            return true;
+        }
+
+        if (leftNode == nullptr || rightNode == nullptr || leftNode->value != rightNode->value) {
+            return false;
+        }
+
+        return isMirror(leftNode->left, rightNode->right) && isMirror(leftNode->right, rightNode->left);
+    }
+
+public:
+    Node* root;
+
+    BinaryTree(Node* starting_node) : root(starting_node) {}
+
+    void addNode(int num){
+        Node* newNode = new Node(num);
+
+        if (root == nullptr){
+            root = newNode;
+            return;
+        }
+
+        Node* curr_Node = root;
+        while (1){
+            if (num >= curr_Node->value){
+                if (curr_Node->right == nullptr){
+                    rewireNode(curr_Node, newNode, 1);
+                    return;
+                }
+                else{
+                    curr_Node = curr_Node->right;
+                }
+            }
+            else{
+                if (curr_Node->left == nullptr){
+                    rewireNode(curr_Node, newNode, 0);
+                    return;
+                }
+                else{
+                    curr_Node = curr_Node->left;
+                }
+            }
+        }
+    }
+
     void delNode(int num){
         Node* deletion_Node = findRecursive(root, num);
 
@@ -128,11 +141,9 @@ public:
             cout << "node not found" << endl;
             return;
         }
-
         // cout << deletion_Node->value << endl;
 
         Node* deletion_Node_parent = deletion_Node->parent;
-
         // cout << deletion_Node_parent->value << endl;
 
         int deletion_Node_direction = 0;
@@ -141,11 +152,9 @@ public:
                 deletion_Node_direction = 1;
             }
         }
-
         // cout << deletion_Node_direction << endl;
 
         Node* successor_Node = findMinSuccessor(deletion_Node);
-
         // cout << successor_Node->value << endl;
 
         if (successor_Node == nullptr){
@@ -162,10 +171,7 @@ public:
         }
 
         Node* successor_Node_parent = successor_Node->parent;
-
         // cout << successor_Node_parent->value << endl;
-
-        // rewireNode(successor_Node->parent, successor_Node->right, 1);
 
         deletion_Node->value = successor_Node->value;
         if (deletion_Node->right == successor_Node){
@@ -175,8 +181,6 @@ public:
             rewireNode(successor_Node_parent, successor_Node->right, 0);
         }
         delete successor_Node;
-
-        // rewireNode(successor_Node, deletion_Node->)
     }
 
     void printTree(Node* node, string prefix = "") {
@@ -190,23 +194,13 @@ public:
         }
     }
 
-    bool isMirror(Node* leftNode, Node* rightNode) {
-        if (leftNode == nullptr && rightNode == nullptr) 
-            return true;
-
-        if (leftNode == nullptr || rightNode == nullptr || leftNode->value != rightNode->value) {
-            return false;
-        }
-
-        return isMirror(leftNode->left, rightNode->right) && isMirror(leftNode->right, rightNode->left);
-    }
-
     bool isSymmetric(Node* root) {
         if (root == nullptr) 
             return true;
         
         return isMirror(root->left, root->right);
     }
+
     void clearTree(Node* node){
         if (node == nullptr){
             return;
@@ -233,17 +227,39 @@ int main(){
 
     string command;
 
+    cout << "========The tree of life protocol========" << endl;
+    cout << "add <num>" << endl;
+    cout << "remove <num>" << endl;
+    cout << "print" << endl;
+    cout << "free" << endl;
+    cout << "symmetric" << endl; 
+    cout << "build_symmetric" << endl;
+    cout << "exit" << endl;
+    cout << "=========================================" << endl;
+
     while (true){
         cin >> command;
 
         if (command == "add"){
             int value;
             cin >> value;
+            if (cin.fail()) {
+                cout << "Please enter number" << endl;
+                cin.clear();
+                cin.ignore(10000, '\n');
+                continue;
+            }
             tree.addNode(value);
         }
         else if (command == "remove"){
             int value;
             cin >> value;
+            if (cin.fail()) {
+                cout << "Please enter number" << endl;
+                cin.clear();
+                cin.ignore(10000, '\n');
+                continue;
+            }
             tree.delNode(value);
         }
         else if (command == "print"){
@@ -275,13 +291,14 @@ int main(){
             Node* left_child = new Node(4);
             Node* right_child = new Node(4);
 
-            tree.rewireNode(tree.root, left, 0);
-            tree.rewireNode(tree.root, right, 1);
+            rewireNode(tree.root, left, 0);
+            rewireNode(tree.root, right, 1);
 
-            tree.rewireNode(left, left_child, 0);
-            tree.rewireNode(right, right_child, 1);
+            rewireNode(left, left_child, 0);
+            rewireNode(right, right_child, 1);
         }
         else if (command == "exit"){
+            tree.clearTree(tree.root);
             break;
         }
         else{
